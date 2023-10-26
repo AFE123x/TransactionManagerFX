e645f8b0e63a55960b2f80a33a2ffb74ab7a4df9
AFE123x <arunf400@gmail.com>
2023-10-26 14:46:46 -0400

    Update one

--- src/main/java/com/example/rubankfx/HelloController.java
+++ src/main/java/com/example/rubankfx/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.rubankfx;
 
+
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.control.DatePicker;
 
 public class HelloController {
+    public Text NameText;
+    public TextField Hengyi;
+    public DatePicker DatePicker;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText(Hengyi.getText());
     }
 }
