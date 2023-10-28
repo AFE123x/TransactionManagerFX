fb71a313229bb34f54d128308ad7570f86710fa9
AFE123x <arunf400@gmail.com>
2023-10-28 10:48:04 -0400

    update

--- src/main/java/com/example/rubankfx/HelloController.java
+++ src/main/java/com/example/rubankfx/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.rubankfx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    public ListView<String> AccountList;
+    public ChoiceBox<String> AccountType;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String [] test = {"one","two","three","four"};
+        AccountType.getItems().addAll(test);
     }
 }
