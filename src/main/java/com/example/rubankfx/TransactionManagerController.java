919000a0a01f9e5947d10b52ba67ece39dce386e
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-01 12:21:47 -0400

    Update TransactionManagerController.java

--- src/main/java/com/example/rubankfx/TransactionManagerController.java
+++ src/main/java/com/example/rubankfx/TransactionManagerController.java
@@ -1,13 +1,22 @@
 package com.example.rubankfx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert;
 
-public class TransactionManagerController {
+public class TransactionManagerController implements Initializable{
+    public CheckBox Loyalty;
     /*
-    This area contains all the components from the Open/Close page.
-     */
+        This area contains all the components from the Open/Close page.
+         */
     AccountDatabase database = new AccountDatabase();
+    @FXML
+    private ToggleGroup campusToggleGroup;
     @FXML
     public TextField InitDeposit;
     @FXML
@@ -26,14 +35,13 @@ public class TransactionManagerController {
     private RadioButton OC_MM;
 
     @FXML
-    private RadioButton Campus_CD;
+    private RadioButton Campus_NB;
     @FXML
-    private RadioButton Campus_CA;
+    private RadioButton Campus_NW;
     @FXML
-    private RadioButton Campus_B;
-    @FXML
-    private RadioButton Campus_L;
+    private RadioButton Campus_C;
     AccountDatabase AccountDatabase = new AccountDatabase();
+
     @FXML
     private void OChandleRadioButtonAction(ActionEvent event) {
         RadioButton selectedRadioButton = (RadioButton) event.getSource();
@@ -54,30 +62,163 @@ public class TransactionManagerController {
             OC_CC.setSelected(false);
             OC_Savings.setSelected(false);
         }
-        Campus_CD.setDisable(!OC_CC.isSelected());
-        Campus_CA.setDisable(!OC_CC.isSelected());
-        Campus_B.setDisable(!OC_CC.isSelected());
-        Campus_L.setDisable(!OC_CC.isSelected());
+        Campus_NB.setDisable(!OC_CC.isSelected());
+        Campus_NW.setDisable(!OC_CC.isSelected());
+        Campus_C.setDisable(!OC_CC.isSelected());
     }
     @FXML
     void OpenAccount(ActionEvent event){
-
-        switch(OCAcctdecision()){
-            case -1:
+        try {
+            boolean added;
+            LocalDate localDate = OC_DOB.getValue();
+            if (localDate == null) {
+                messageListView.getItems().add("Please enter a valid date! ");
                 return;
-            case 0:// Checkings Account.
-                //CC,John,Doe,2/19/2000,599.99,0
-                String [] array = {"CC",OC_First_Name.getText(),OC_Last_Name.getText(),InitDeposit.getText(), OC_DOB.getValue().toString().replace("-", "/")};
-                database.open(Checking.makeChecking(array));
-                System.out.println(Checking.makeChecking(array));
-                break;
-
-
+            }
+            Date date = new Date(localDate.getYear(), localDate.getMonthValue(), localDate.getDayOfMonth());
+            if (!date.isValid()) {
+                messageListView.getItems().add(date.getLastMessage());
+                return;
+            }
+            Profile profile = makeProfile(date);
+            double balance;
+            try {
+                balance = Double.parseDouble(InitDeposit.getText());
+                if (balance <= 0) {
+                    messageListView.getItems().add("Initial deposit must be greater than zero!");
+                    return;
+                }
+            } catch (NumberFormatException e) {
+                messageListView.getItems().add("Please enter a valid number for initial deposit");
+                return;
+            }
+            int decision = OCAcctdecision();
+            switch (decision) {
+                case 0: // Checking Account
+                    Checking checkingAccount = Checking.makeChecking(profile, balance);
+                    added = database.open(checkingAccount);
+                    if (!added && database.getLastMessage().equals("AE")) {
+                        messageListView.getItems().add("Account already exists!");
+                    }
+                    else {
+                        messageListView.getItems().add("Account opened successfully!");
+                    }
+                    break;
+                case 1: //College Checking Account
+                    Integer campusCodeInt = validateAndGetCampusCode();
+                    if (campusCodeInt == null) {
+                        return; // Exit since either no code was selected or it was invalid.
+                    }
+                    if (!date.checkCollegeCheckingValidity()) {
+                        messageListView.getItems().add(date.getLastMessage());
+                        return;
+                    }
+                    try {
+                        CollegeChecking collegeChecking = CollegeChecking.makeCollegeChecking(profile, balance, campusCodeInt);
+                        added = database.open(collegeChecking);
+                        if (!added && database.getLastMessage().equals("AE")) {
+                            messageListView.getItems().add("Account already exists!");
+                        }
+                        else {
+                            messageListView.getItems().add("Account opened successfully!");
+                        }
+                    } catch (IllegalArgumentException e) {
+                        showAlert("Error", e.getMessage());
+                    }
+                    break;
+                case 2: // Savings Account
+                    boolean loyal;
+                    loyal = Loyalty.isSelected();
+                    try{
+                        Savings Saving = Savings.makeSavings(profile,balance,loyal);
+                        added = database.open(Saving);
+                        if(!added && database.getLastMessage().equals("AE")){
+                            messageListView.getItems().add("Account already exists!");
+                        }
+                        else {
+                            messageListView.getItems().add("Account opened successfully!");
+                        }
+                    }catch (IllegalArgumentException e){
+                        showAlert("Error",e.getMessage());
+                    }
+                    break;
+                case 3: //Money Market
+                    if(balance < 2000) {
+                        messageListView.getItems().add("Balance cannot be below 2000!");
+                        return;
+                    }
+                    try{
+                        MoneyMarket Money = MoneyMarket.makeMoneyMarket(profile, balance);
+                        added = database.open(Money);
+                        if(!added && database.getLastMessage().equals("AE")){
+                            messageListView.getItems().add("Account already exists!");
+                        }
+                        else {
+                            messageListView.getItems().add("Account opened successfully!");
+                        }
+                    }
+                    catch (IllegalArgumentException e){
+                        showAlert("Error",e.getMessage());
+                    }
+                    break;
+            }
+        }catch(Exception e) {
+            showAlert("Error", e.getMessage());
         }
     }
+
+    private Integer validateAndGetCampusCode() {
+        String campusCode = getSelectedCampusCode();
+        if (campusCode == null) {
+            messageListView.getItems().add("No campus code selected! Please select a valid code! ");
+            return null;
+        }
+
+        try {
+            return Integer.parseInt(campusCode);
+        } catch (NumberFormatException e) {
+            showAlert("Error", "Invalid campus code.");
+            return null;
+        }
+    }
+
+
+    private Profile makeProfile(Date date) {
+        String firstName = OC_First_Name.getText();
+        String lastName = OC_Last_Name.getText();
+        return new Profile(firstName, lastName, date);
+    }
+
+
+    private void showAlert(String error, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void addMessageToListView(String message) {
+        messageListView.getItems().add(message);
+        messageListView.scrollTo(messageListView.getItems().size() - 1);
+    }
+
+
+    private String getSelectedCampusCode() {
+        if (Campus_NB.isSelected()) {
+            return "0";
+        } else if (Campus_NW.isSelected()) {
+            return "1";
+        } else if (Campus_C.isSelected()) {
+            return "2";
+        }
+        return null;
+    }
+
+
     @FXML
     void CloseAccount(ActionEvent event){
-        System.out.println("Closing Account");
+        //toDO
     }
     @FXML
     private int OCAcctdecision() {
@@ -94,9 +235,6 @@ public class TransactionManagerController {
     }
 
 
-
-
-
 @FXML
     private Button Open;
     @FXML
@@ -104,8 +242,12 @@ public class TransactionManagerController {
     @FXML
     private Button Clear;
 
+    public void ClearScreen(ActionEvent actionEvent) {
+        messageListView.getItems().clear();
+    }
+
     @FXML
-    private ListView<?> listView1; // Replace with the correct type
+    private ListView<String> messageListView;; // Replace with the correct type
 
     //DEPOSIT AND WITHDRAW STUFF
     @FXML
@@ -151,8 +293,25 @@ public class TransactionManagerController {
     @FXML
     private Button Withdraw;
 
+    public ToggleGroup getCampusToggleGroup() {
+        return campusToggleGroup;
+    }
+
+
+
     @FXML
-    private ListView<String> listView2; // Replace with the correct type
+    private ListView<String> accountsListView; // Replace with the correct type
+
+    @FXML
+    private void handlePrintAccountsAction(ActionEvent event) {
+        Account[] allAccounts = database.getAllAccounts();
+        accountsListView.getItems().clear();
+
+        for (Account account : allAccounts) {
+            accountsListView.getItems().add(account.toString());
+        }
+    }
+
 
     @FXML
     private Button Print_Account;
@@ -167,5 +326,12 @@ public class TransactionManagerController {
     private ListView<String> listView3; // Replace with the correct type
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        campusToggleGroup = new ToggleGroup();
+        Campus_NB.setToggleGroup(campusToggleGroup);
+        Campus_NW.setToggleGroup(campusToggleGroup);
+        Campus_C.setToggleGroup(campusToggleGroup);
+    }
 
 }
